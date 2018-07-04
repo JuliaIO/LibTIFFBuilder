fb698dc8d24c9b80849fbe7d3949c1ddd495ca7a
SimonDanisch <sdanisch@gmail.com>
2018-07-04 14:09:37 +0200

    Add autogenerated files to build libtiff

---
+++ README.md
@@ -0,0 +1,9 @@
+# LibTIFFBuilder
+
+[![Build Status](https://travis-ci.org/SimonDanisch/LibTIFFBuilder.svg?branch=master)](https://travis-ci.org/SimonDanisch/LibTIFFBuilder)
+
+This repository builds binary artifacts for the libtiff project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/SimonDanisch/LibTIFFBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,59 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libtiff"
+version = v"4.0.9"
+
+# Collection of sources required to build libtiff
+sources = [
+    "https://gitlab.com/libtiff/libtiff.git" =>
+    "020bd2fd3b4235b2175dafa9633d2b191c50e7cd",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd libtiff/
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
+make
+make install
+exit
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc, :eabihf),
+    Linux(:powerpc64le, :glibc),
+    Linux(:i686, :musl),
+    Linux(:x86_64, :musl),
+    Linux(:aarch64, :musl),
+    Linux(:armv7l, :musl, :eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libtiff", :libtiff),
+    LibraryProduct(prefix, "libtiffxx", :libtiffxx)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.1/build_Zlib.v1.2.11.jl",
+    "https://github.com/SimonDanisch/LibpngBuilder/releases/download/v1.6.31/build_libpng.v1.0.0.jl",
+    "https://github.com/SimonDanisch/LibJPEGBuilder/releases/download/v9b/build_libjpeg.v9.0.0-b.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
